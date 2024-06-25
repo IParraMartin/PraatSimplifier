1a33027adfcb67ff388e82f0e1993236f5efe9fd
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2024-06-25 10:18:03 -0500

    New changes to functions for better user experience

--- praat_simplifier.py
+++ praat_simplifier.py
@@ -14,14 +14,32 @@ except ImportError as e:
 
 class PraatSimplifier():
     
-    def __init__(self):
-        self.in_dir = None
+    def __init__(self, in_dir: str, out_dir: str):
+        self.in_dir = in_dir
+        self.out_dir = out_dir
         self.n_timestamps = None
         self.n_formants = None
         self.f_data = []
 
 
-    def get_formants(self, in_dir: str, n_timestamps: int = 10, n_formants: int = 3) -> list:
+    def save_to_mono(self):
+
+        """
+        Convert all files to mono if necessary.
+        """
+
+        for file in os.listdir(self.in_dir):
+            if file.endswith('.wav'):
+                sound = parselmouth.Sound(os.path.join(self.in_dir, file))
+                mono_signal = sound.convert_to_mono()
+                output_path = os.path.join(self.out_dir, file)
+                mono_signal.save(output_path, format='WAV')
+                print(f'Mono sound saved to {output_path}')
+        print('Done.')
+        return mono_signal
+
+
+    def get_formants(self, n_timestamps: int = 10, n_formants: int = 3) -> list:
         
         """
         Extract formants from audio files in the specified directory.
@@ -35,7 +53,6 @@ class PraatSimplifier():
         list: A list of dictionaries containing formant data.
         """
         
-        self.in_dir = in_dir
         self.n_timestamps = n_timestamps
         self.n_formants = n_formants
         self.f_data = []
@@ -46,18 +63,12 @@ class PraatSimplifier():
                 sound = parselmouth.Sound(os.path.join(self.in_dir, file))
                 sound = sound.convert_to_mono()
                 dur = sound.get_total_duration()
-                """
-                Set the amount of timestamps predefined in the class
-                """
+
                 time_points = np.linspace(0, dur, self.n_timestamps)
 
                 formant = sound.to_formant_burg(max_number_of_formants=self.n_formants)
 
                 for time in time_points:
-                    """
-                    Loop using the amount of formants to extract predefined in
-                    n_formants. Delete the last four chars (.wav, .mp3, .mp4)
-                    """
                     formant_data = {'sound': file[:-4], 'time': time}
                     for i in range(1, self.n_formants + 1):
                         formant_value = formant.get_value_at_time(i, time)
@@ -67,7 +78,7 @@ class PraatSimplifier():
         return self.f_data
     
 
-    def export_formants(self, out_dir: str = None):
+    def export_formants(self):
         
         """
         Export the extracted formant data to a CSV file.
@@ -81,12 +92,12 @@ class PraatSimplifier():
             return
         
         df = pd.DataFrame(self.f_data)
-        file_path = f'{out_dir}/formants.csv' if out_dir else 'formants.csv'
+        file_path = f'{self.out_dir}/formants.csv' if self.out_dir else 'formants.csv'
         df.to_csv(file_path, index=False)
         print(f'File saved to {file_path}')
 
 
-    def plot_formants(self, plot_out_dir: str = './', save_plot: bool = False, dpi: int = 1200):
+    def plot_formants(self, save_plot: bool = False, dpi: int = 1200):
 
         """
         Plot the extracted F-values by sound (maximum of 9 for clarity.)
@@ -118,9 +129,9 @@ class PraatSimplifier():
             axs[i].set_visible(False)
 
         if save_plot:
-            if not os.path.exists(plot_out_dir):
-                os.makedirs(plot_out_dir)
-            file_path = os.path.join(plot_out_dir, 'formant_plots.png')
+            if not os.path.exists(self.out_dir):
+                os.makedirs(self.out_dir)
+            file_path = os.path.join(self.out_dir, 'formant_plots.png')
             try:
                 plt.savefig(file_path, dpi=dpi)
                 print(f"Plot saved successfully to {file_path}")
@@ -133,16 +144,16 @@ class PraatSimplifier():
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Simplified code to extract formant values in a .csv file.')
-    parser.add_argument('--sounds_dir', type=str, required=True, help='Directory to your sound files.')
+    parser.add_argument('--in_dir', type=str, required=True, help='Directory to your sound files.')
+    parser.add_argument('--out_dir', type=str, required=False, default='./', help='Output directory for the .csv file.')
     parser.add_argument('--n_timestamps', type=int, required=False, default=10, help='Number of timestamps to extract the formants from.')
     parser.add_argument('--n_formants', type=int, required=False, default=3, help='Number of formants to extract.')
-    parser.add_argument('--out_dir', type=str, required=False, default='./', help='Output directory for the .csv file.')
     parser.add_argument('--save_plot', type=bool, required=False, default=False, help='True = save plot; False = do not save')
-    parser.add_argument('--plot_out_dir', type=str, required=False, default='./', help='Output directory for the .csv file.')
     parser.add_argument('--dpi', type=int, required=False, default=1200, help='Quality of plot.')
+    parser.add_argument('--save_to_mono', type=bool, required=False, default=False, help='Save to mono all the sounds in the in_dir.')
     args = parser.parse_args()
 
-    simplifier = PraatSimplifier()
+    simplifier = PraatSimplifier(args.in_dir, args.out_dir)
 
     formants = simplifier.get_formants(
         in_dir=args.sounds_dir,
@@ -153,5 +164,9 @@ if __name__ == "__main__":
     simplifier.export_formants(out_dir=args.out_dir)
 
     if args.save_plot:
-        plot_out_dir = args.plot_out_dir if args.plot_out_dir else './'
+        plot_out_dir = args.out_dir if args.out_dir else './'
         simplifier.plot_formants(plot_out_dir=plot_out_dir, save_plot=True, dpi=args.dpi)
+
+    if args.save_to_mono:
+        simplifier.save_to_mono()
+    
